aef604919ee0f8b3c294fa46fee0804cfe14a1af
AK1R4S4T0H <deavers.chadwick@gmail.com>
2023-05-14 03:37:53 -0400

    fix

--- docs/main.js
+++ docs/main.js
@@ -1,49 +1,35 @@
-const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:;<>[]{}()/?@#$%^&*|";
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789:;<>[]{}()/?@#$%^&*|"
 
 let interval = null;
-let timeout = 0;
 
-function startRandomHover() {
-  clearTimeout(timeout);
+document.querySelector("h1").onmouseover = event => {  
+  let iteration = 0;
+  
   clearInterval(interval);
-
-  timeout = setTimeout(() => {
-    const h1Element = document.querySelector("h1");
-    const originalText = h1Element.dataset.value || h1Element.innerText;
-
-    let iteration = 0;
-
-    interval = setInterval(() => {
-      h1Element.innerText = originalText
-        .split("")
-        .map((letter, index) => {
-          if (index < iteration) {
-            return originalText[index];
-          }
-
-          return letters[Math.floor(Math.random() * letters.length)];
-        })
-        .join("");
-
-      if (iteration >= originalText.length) {
-        clearInterval(interval);
-      }
-
-      iteration += 1 / 7;
-    }, 60);
-
-    startRandomHover(); // Trigger another random hover after completion
-  }, getRandomDuration(2, 7) * 1000);
+  
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if(index < iteration) {
+          return event.target.dataset.value[index];
+        }
+      
+        return letters[Math.floor(Math.random() * 56)]
+      })
+      .join("");
+    
+    if(iteration >= event.target.dataset.value.length){ 
+      clearInterval(interval);
+    }
+    
+    iteration += 1 / 7;
+  }, 60);
 }
 
-function getRandomDuration(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-startRandomHover(); // Start the initial random hover
 
 var autoPlayVideo = document.getElementById("iframe");
-autoPlayVideo.oncanplaythrough = function() {
-  autoPlayVideo.muted = false;
-  autoPlayVideo.play();
-};
+    autoPlayVideo.oncanplaythrough = function() {
+        autoPlayVideo.muted = false;
+        autoPlayVideo.play();
+    }
